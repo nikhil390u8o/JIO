c511024cf2d744bae74b10baf71fe52df11144a8
nikhil390u8o <sangeetasharma1005sa@gmail.com>
2026-05-02 12:32:57 +0530

    Update thumbnail.py

--- thumbnail.py
+++ thumbnail.py
@@ -4,42 +4,47 @@ import io, textwrap
 FONT_PATH = "fonts/BraveThorn-Regular.ttf"
 BG_IMAGE = "BG.jpg"
 
-def generate_thumb(song, artist, duration):
+def sec_to_time(sec):
+    sec = int(sec)
+    m = sec // 60
+    s = sec % 60
+    return f"{m}:{s:02d}"
+
+def generate_thumb(song_json):
+    # Required fields from JSON
+    song = song_json.get("song", "Unknown Song")
+    artist = song_json.get("primary_artists", "Unknown Artist")
+    duration = sec_to_time(song_json.get("duration", 0))
+
     img = Image.open(BG_IMAGE).convert("RGB")
     W, H = img.size
     draw = ImageDraw.Draw(img)
 
-    # Font size auto-scale based on width
-    title_size = int(W * 0.065)   # responsive
-    info_size = int(W * 0.035)
+    # Responsive font
+    title_font = ImageFont.truetype(FONT_PATH, int(W * 0.065))
+    info_font = ImageFont.truetype(FONT_PATH, int(W * 0.035))
 
-    title_font = ImageFont.truetype(FONT_PATH, title_size)
-    info_font = ImageFont.truetype(FONT_PATH, info_size)
-
-    # Wrap song title
+    # Wrap title
     wrap_width = int(W / 70)
     lines = textwrap.wrap(song, width=wrap_width)
 
-    total_height = 0
-    bboxes = []
-
+    total_h = 0
+    dims = []
     for line in lines:
         box = draw.textbbox((0, 0), line, font=title_font)
         h = box[3] - box[1]
-        total_height += h + 20
-        bboxes.append((line, box, h))
+        total_h += h + 20
+        dims.append((line, box, h))
 
-    # Start vertically centered
-    y = (H - total_height) / 2
+    y = (H - total_h) / 2
 
-    # Draw title
-    for line, box, h in bboxes:
-        text_w = box[2] - box[0]
-        x = (W - text_w) / 2
+    # Draw song title
+    for line, box, h in dims:
+        x = (W - (box[2] - box[0])) / 2
         draw.text((x, y), line, font=title_font, fill="#39ff14")
         y += h + 20
 
-    # Artist + duration
+    # Draw artist + duration
     info = f"{artist}  •  {duration}"
     box = draw.textbbox((0, 0), info, font=info_font)
     x = (W - (box[2] - box[0])) / 2
